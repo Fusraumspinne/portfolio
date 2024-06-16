7397bb08ac0428344081ebbbee9dfd8df7d3a118
Fusraumspinne <mjjaeger2002@gmail.com>
2024-06-16 19:07:52 +0200

    client and customer stuff

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { InfiniteMovingCards } from './ui/InfinitMovingCards'
-import { testimonials } from '@/data'
+import { companies, testimonials } from '@/data'
+import { div } from 'three/examples/jsm/nodes/Nodes.js'
 
 const Clients = () => {
   return (
@@ -9,10 +10,17 @@ const Clients = () => {
         Kind words from {' '}
         <span className='text-purple'> satisfied clients</span>
       </h1>
-      <div className='flex flex-col items-center'>
-        <div className='h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center relative overflow-hidden'>
+      <div className='flex flex-col items-center max-lg:mt-10'>
             <InfiniteMovingCards items={testimonials} direction='right' speed='slow'/>
-        </div>
+
+            <div className='flex flex-wrap items-center justify-center gap-4 md:gap-16 mt-10'>
+                {companies.map(({id, img, name, nameImg}) => (
+                    <div key={id} className='flex md:max-w-60 max-w-32 gap-2'>
+                        <img src={img} alt={name}  className='md:w-10 w-5'/>
+                        <img src={nameImg} alt={name}  className='md:w-24 w-20'/>
+                    </div>
+                ))}
+            </div>
       </div>
     </div>
   )
